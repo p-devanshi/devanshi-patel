3a2e62019a4e19a42eb8c436a3a44cf294f27c80
Patel Devanshi <125117035+p-devanshi@users.noreply.github.com>
2024-06-18 18:43:21 +0530

    Add files via upload

---
+++ Inheritance/BaseClass.java
@@ -0,0 +1,20 @@
+package Inheritance;
+
+public class BaseClass {
+	private int amt;
+
+	public int getAmt() {
+		return amt;
+	}
+
+	public void setAmt(int amt) {
+		this.amt = amt;
+	}
+
+	@Override
+	public String toString() {
+		return "BaseClass [amt=" + amt + "]";
+	}
+	
+
+}
---
+++ Inheritance/DemoMain.java
@@ -0,0 +1,13 @@
+package Inheritance;
+
+public class DemoMain {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		ChildClass c=new ChildClass();
+		c.setAmt(1000);
+		System.out.println(c);
+	}
+
+}
