bc7949c5fe4d7522ca88ee94b8ad30c4d6e609b9
Patel Devanshi <125117035+p-devanshi@users.noreply.github.com>
2024-06-18 18:41:30 +0530

    Add files via upload

---
+++ count_tot_object/CntObj.java
@@ -0,0 +1,15 @@
+package count_tot_object;
+
+public class CntObj {
+	private static int cnt;
+
+	public CntObj() {
+		cnt++;
+	}	
+	public static void CntTotObj()
+	{
+		System.out.println("Total objects of CntObh Class is "+cnt);
+	}
+	
+
+}
---
+++ count_tot_object/MainClass.java
@@ -0,0 +1,17 @@
+package count_tot_object;
+
+public class MainClass {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		CntObj ob1=new CntObj();
+		CntObj ob2=new CntObj();
+		CntObj ob3=new CntObj();
+		CntObj ob4=new CntObj();
+		CntObj ob5=new CntObj();
+		
+		CntObj.CntTotObj();
+
+	}
+
+}
